a3710bf1ca57617b7dfe5fdffa8337ff1474a7d3
Ishan <ishanwijes@gmail.com>
2023-02-07 02:22:00 +1100

    added changes to the views

--- Expense Tracker New/Expense Tracker/Expense Tracker/Program.cs
+++ Expense Tracker New/Expense Tracker/Expense Tracker/Program.cs
@@ -14,6 +14,9 @@ builder.Services.AddControllersWithViews();
 // DI
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+// Register Syncfusion license so that annoying message at the top disappears
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBaFt/QHRqVVhkVFpGaV5BQmFJfFBmQmlYd1RzfUU3HVdTRHRcQl9jT35Ud01gUH9ZdXY=");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
